6d5f1d819418b1613dcb3221dff57d0bac98588d
Jvilla125 <Jvilla125@gmail.com>
2022-08-29 11:53:40 -0700

    The llamas is imported to the main_app views.py page and I have access to the database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Llama(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    
+    
+    
+    
+# class Llama:
+#     def __init__(self, name, breed, description):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+
+# llamas = [
+#     Llama('Carl', 'Classic Llama', 'Classic llamas have double-coated fleece and are much larger than other llamas.'),
+#     Llama('Spence', 'Wooly Llama', 'Wooly llamas are smaller than other llamas and have strong wool covering their body.'),
+#     Llama('Ken', 'Vicuna Llama', 'Vicuna Llamas coats are orange colored and have rare and valuable wool.')
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
+from .models import Llama
 
 # Create your views here.
 from django.http import HttpResponse
-
-class Llama:
-    def __init__(self, name, breed, description):
-        self.name = name
-        self.breed = breed
-        self.description = description
-
-llamas = [
-    Llama('Carl', 'Classic Llama', 'Classic llamas have double-coated fleece and are much larger than other llamas.'),
-    Llama('Spence', 'Wooly Llama', 'Wooly llamas are smaller than other llamas and have strong wool covering their body.'),
-    Llama('Ken', 'Vicuna Llama', 'Vicuna Llamas coats are orange colored and have rare and valuable wool.')
-]
 
 def home(request):
     return HttpResponse('<center><h1>Hello World</center></h1>')
@@ -22,4 +11,5 @@ def about(request):
     return render(request, 'about.html')
 
 def llamas_index(request):
+    llamas = Llama.objects.all()
     return render(request, 'llamas/index.html', {'llamas': llamas})
